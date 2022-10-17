f9bc89a1a1ecdc69fa8d25a542938748986744c9
Tristan Ross <tross@ezoic.com>
2022-10-17 09:06:44 -0700

    chore: use sdk's mkflake function

--- flake.nix
+++ flake.nix
@@ -5,30 +5,5 @@
     url = github:ExpidusOS/sdk;
   };
 
-  outputs = { self, expidus-sdk }:
-    {
-      overlays.default = final: prev: {
-        libdevident = (prev.libdevident.overrideAttrs (old: {
-          version = self.rev or "dirty";
-          src = builtins.path { name = "libdevident"; path = prev.lib.cleanSource ./.; };
-        }));
-      };
-
-      packages = expidus-sdk.lib.forAllSystems (system:
-        let
-          pkgs = expidus-sdk.lib.nixpkgsFor.${system};
-        in {
-          default = (self.overlays.default pkgs pkgs).libdevident;
-        });
-
-      devShells = expidus-sdk.lib.forAllSystems (system:
-        let
-          pkgs = expidus-sdk.lib.nixpkgsFor.${system};
-          pkg = self.packages.${system}.default;
-        in {
-          default = pkgs.mkShell {
-            packages = pkg.nativeBuildInputs ++ pkg.buildInputs;
-          };
-        });
-    };
+  outputs = { self, expidus-sdk }: expidus-sdk.lib.mkFlake { inherit self; name = "libdevident"; };
 }
