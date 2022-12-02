edfef7d77bb2b08116de9d0d1ecdd89e5b431e75
Tristan Ross <tross@ezoic.com>
2022-12-02 10:20:35 -0800

    chore: update flake

--- flake.nix
+++ flake.nix
@@ -5,5 +5,10 @@
     url = github:ExpidusOS/sdk;
   };
 
-  outputs = { self, expidus-sdk }: expidus-sdk.lib.expidus.flake.makeOverride { inherit self; name = "libdevident"; };
+  outputs = { self, expidus-sdk }:
+    with expidus-sdk.lib;
+    expidus.flake.makeOverride {
+      inherit self;
+      name = "libdevident";
+    };
 }
