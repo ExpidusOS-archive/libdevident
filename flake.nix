71a19a753edfceabae6561f28e116a3c7804769c
Tristan Ross <tross@ezoic.com>
2022-12-02 10:35:14 -0800

    fix: check now works

--- flake.nix
+++ flake.nix
@@ -7,8 +7,15 @@
 
   outputs = { self, expidus-sdk }:
     with expidus-sdk.lib;
+    let
+      unsupportedSystems = builtins.map (name: "${name}-cygwin") [ "i686" "x86_64" ];
+      defaultSupported = lists.flatten (builtins.attrValues expidus.system.defaultSupported);
+    in
     expidus.flake.makeOverride {
       inherit self;
       name = "libdevident";
+      sysconfig = expidus.system.make {
+        supported = lists.subtractLists unsupportedSystems defaultSupported;
+      };
     };
 }
